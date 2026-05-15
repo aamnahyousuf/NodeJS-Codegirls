23536370bc871e6498056f47feeace704f92e1a2
aamnahyousuf <aamnahyousuf56@gmail.com>
2026-05-15 13:22:19 +0500

    app.js

--- Process stdin on/app.js
+++ Process stdin on/app.js
@@ -1,10 +1,34 @@
-process.stdin.on("data", function(input) {
-    let numbers = input.toString().split(" ");
-    
-    let a = Number(numbers[0]);
-    let b = Number(numbers[1]);
+let inputs = [];
+let num1, num2, op;
 
-    console.log(a + b);
+console.log("Enter first number:");
 
+process.stdin.on("data", (input) => {
+  input = input.toString().trim();
 
+  inputs.push(input);
+
+  if (inputs.length === 1) {
+    num1 = Number(inputs[0]);
+    console.log("Enter second number:");
+  }
+
+  else if (inputs.length === 2) {
+    num2 = Number(inputs[1]);
+    console.log("Enter operation (+, -, *, /):");
+  }
+
+  else if (inputs.length === 3) {
+    op = inputs[2];
+
+    let result;
+
+    if (op === "+") {result = num1 + num2;}
+    else if (op === "-") {result = num1 - num2;}
+    else if (op === "*") {result = num1 * num2;}
+    else if (op === "/") {result = num1 / num2;}
+
+    console.log("Result:", result);
+    process.exit();
+  }
 });
