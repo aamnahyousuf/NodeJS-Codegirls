e7428937fe044f4758a8bb2408a0266aff2d5c41
aamnahyousuf <aamnahyousuf56@gmail.com>
2026-05-18 13:14:53 +0500

    app.js

--- Create Server Assignment/app.js
+++ Create Server Assignment/app.js
@@ -1,10 +1,24 @@
-const http = require("http");
+import http from "http";
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = new URL(req.url, "http://localhost:8000");
+
+  const name = parsedUrl.searchParams.get("name");
+  const fName = parsedUrl.searchParams.get("fname");
 
   res.writeHead(200, { "Content-Type": "text/plain" });
 
+  if (!(name && fName)) {
+    res.end(
+      "Please provide your name and father name like ?name=ali&fname=ali"
+    );
+  } else if (name === "ali" && fName === "ali") {
+    res.end("You are allowed");
+  } else {
+    res.end("You are not");
+  }
 });
+
 server.listen(8000, () => {
-  console.log("Server Created!");
+  console.log("Server running at http://localhost:8000");
 });
