d9c9f5501869678a04acee0d321f51fc6fccc116
aamnahyousuf <aamnahyousuf56@gmail.com>
2026-06-12 13:45:34 +0500

    Classes Assignment

---
+++ Classes Assignment/app.js
@@ -0,0 +1,22 @@
+class Vehicle{
+    constructor(brand, model){
+        this.brand = brand;
+        this.model = model;
+    }
+    start(){
+        return "Car started: Vroom Vroom!";
+    }
+}
+
+class Car extends Vehicle {
+    constructor(brand, model, fuel){
+        super(brand, model);
+        this.fuelType = fuel;
+    }
+    carInfo(){
+        return [this.brand, this.model, this.fuelType];
+    }
+}
+
+const Mercy = new Car ("Merecedes", 2014, "Diesel")
+console.log(Mercy.carInfo());
